430ea4276a8db1afb82e54aafa49833ab6bb1419
devKamleshParihar <136742378+devKamleshParihar@users.noreply.github.com>
2023-12-16 12:31:16 +0000

    guess number  using html and js

--- Counter/counter.js
+++ Counter/counter.js
@@ -1,17 +1,4 @@
-// let cval = document.querySelector('#counterVal')
-// cval.innerText = 2
 
-/*
-
-let d = document.querySelector('#d');
-let fun = ()=>{
-    cv.innerText = cv.innerText - 1;
-}
-d.onclick = fun()
-let cv = document.querySelector('#cv');
-let i = document.querySelector('#i');
-
-*/
 let cv = document.querySelector('#cv');
 let d = document.querySelector('#d');
 let r = document.querySelector('#r');
@@ -30,8 +17,6 @@ let res = ()=>{
 
 i.onclick = inr;
 d.onclick = dec;
-// r.onclick = res;
-
 r.addEventListener('click',res)
 
 
